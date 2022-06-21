a8319a736024a59ab02b95b7d5265a5aacad68c9
MarkoGitLab <dan.m.markovits@gmail.com>
2022-06-21 19:34:13 +0300

    Added the ability to press enter to log in

--- app/src/login.js
+++ app/src/login.js
@@ -11,11 +11,13 @@ function Login() {
     Axios.defaults.withCredentials = true
 
     const login = () => {
+        console.log(password)
         Axios.post("http://localhost:8080/login", {
             password: password
         }).then((res) => {
             console.log(res)
             if (res.data.message) {
+                console.log(res.data.message)
                 alert("oof")
             }
             else {
@@ -26,14 +28,18 @@ function Login() {
     }
 
     const changePassword = (e) => {
+        console.log(e.target.value)
         setPassword(e.target.value)
+        if (e.key === "Enter") {
+            login()
+        }
     }
 
     return (
         <div className="center">
             <div>
                 <h1>Login</h1>
-                <input type="text" placeholder="passwrod...." onChange={changePassword}/>
+                <input type="text" placeholder="passwrod...." onKeyUp={changePassword}/>
                 <button onClick={login}> Login </button>
             </div>
         </div>
